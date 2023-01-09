2508b7955688d267c86851959bd48d8e7d4eef3a
Josh Siegel <jsiegel@jumptrading.com>
2023-01-09 16:40:14 +0000

    sui/recover: more full node fixes

--- sui/scripts/start_node.sh
+++ sui/scripts/start_node.sh
@@ -9,6 +9,6 @@ sleep 10
 #sleep 2
 #curl -X POST -d '{"FixedAmountRequest":{"recipient": "'"0x2acab6bb0e4722e528291bc6ca4f097e18ce9331"'"}}' -H 'Content-Type: application/json' http://127.0.0.1:5003/gas
 sed -i -e 's/:9000/:9002/' ~/.sui/sui_config/fullnode.yaml
+echo 'websocket-address: "0.0.0.0:9001"' >> ~/.sui/sui_config/fullnode.yaml
 sui-node --config-path ~/.sui/sui_config/fullnode.yaml
-
-#sleep infinity
+# sleep infinity
